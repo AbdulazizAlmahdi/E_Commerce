6ccc2fdec43aa1d4068db0131e9e279923bda818
NADER006 <naderthabit06@gmail.com>
2022-08-31 20:15:58 +0300

    upload  the editing in frontend design

--- wwwroot/js/detailes.js
+++ wwwroot/js/detailes.js
@@ -1,4 +1,80 @@
-var btnCopy =$("#copybtn"),
+//====================== Evaluationproduct (star rating) =============
+$(document).ready(function () {
+    $("#st1").click(function () {
+        $(".Evaluationproduct .fa-star").css("color", "#BCBCBC");
+        $("#st1").css("color", "#FFE622 ");
+        $(".Evaluationproduct li").val('1');
+
+
+    });
+    $("#st2").click(function () {
+        $(".Evaluationproduct .fa-star").css("color", "#BCBCBC");
+        $("#st1, #st2").css("color", "#FFE622 ");
+        $(".Evaluationproduct li").val('2');
+
+
+    });
+    $("#st3").click(function () {
+        $(".Evaluationproduct .fa-star").css("color", "#BCBCBC")
+        $("#st1, #st2, #st3").css("color", "#FFE622 ");
+        $(".Evaluationproduct li").val('3');
+
+    });
+    $("#st4").click(function () {
+        $(".Evaluationproduct .fa-star").css("color", "#BCBCBC");
+        $("#st1, #st2, #st3, #st4").css("color", "#FFE622 ");
+        $(".Evaluationproduct li").val('4');
+
+    });
+    $("#st5").click(function () {
+        $(".Evaluationproduct .fa-star").css("color", "#BCBCBC");
+        $("#st1, #st2, #st3, #st4, #st5").css("color", "#FFE622 ");
+        $(".Evaluationproduct li").val('5');
+
+
+    });
+
+});
+//====================== Evaluationaution (star rating) =============
+$(document).ready(function () {
+    $("#st1").click(function () {
+        $(".Evaluationaution .fa-star").css("color", "#BCBCBC");
+        $("#st1").css("color", "#FFE622 ");
+        $(".Evaluationaution li").val('1');
+
+
+    });
+    $("#st2").click(function () {
+        $(".Evaluationaution .fa-star").css("color", "#BCBCBC");
+        $("#st1, #st2").css("color", "#FFE622 ");
+        $(".Evaluationaution li").val('2');
+
+
+    });
+    $("#st3").click(function () {
+        $(".Evaluationaution .fa-star").css("color", "#BCBCBC")
+        $("#st1, #st2, #st3").css("color", "#FFE622 ");
+        $(".Evaluationaution li").val('3');
+
+    });
+    $("#st4").click(function () {
+        $(".Evaluationaution .fa-star").css("color", "#BCBCBC");
+        $("#st1, #st2, #st3, #st4").css("color", "#FFE622 ");
+        $(".Evaluationaution li").val('4');
+
+    });
+    $("#st5").click(function () {
+        $(".Evaluationaution .fa-star").css("color", "#BCBCBC");
+        $("#st1, #st2, #st3, #st4, #st5").css("color", "#FFE622 ");
+        $(".Evaluationaution li").val('5');
+
+
+    });
+
+});
+
+
+var btnCopy = $("#copybtn"),
     numberorder =$("#numberorder"),
     copybtnmessage =$("#copybtnmessage");
   
